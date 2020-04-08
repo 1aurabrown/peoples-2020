6cb87cf7ce0a4276079c60d3991d3377b3302a62
Laura Brown <laura@laurabrown.xyz>
2020-04-08 17:31:05 +0200

    some improvements to search form

--- src/scripts/components/search-form.js
+++ src/scripts/components/search-form.js
@@ -1,35 +1,69 @@
 const selectors = {
-  button: 'a[href="/search"]'
+  button: '.search-form__button',
+  input: 'input[type=search]',
+  form: '.search-form__form',
+  clear: '.search-form__clear'
 }
-
-const namespace = '.search-form'
 
 export default class SearchForm {
   constructor (el) {
     this.el = el
-    this.showForm = this._showForm.bind(this)
-    this.hideForm = this._hideForm.bind(this)
+    this.clickedCTA = this._clickedCTA.bind(this)
+    this.clickedOutside = this._clickedOutside.bind(this)
+    this.clickedClear = this._clickedClear.bind(this)
+    this.clickedInside = this._clickedInside.bind(this)
+
     this.button = this.el.querySelector(selectors.button)
-    console.log(this.button)
-    this.button.addEventListener('click', this.showForm)
+    this.form = this.el.querySelector(selectors.form)
+    this.input = this.el.querySelector(selectors.input)
+    this.clear = this.el.querySelector(selectors.clear)
+    this.el.addEventListener('click', this.clickedInside)
+    this.button.addEventListener('click', this.clickedCTA)
+    this.clear.addEventListener('click', this.clickedClear)
+  }
+
+  show() {
+    this.clearInput()
+    this.el.classList.add('active')
+    window.addEventListener('click', this.clickedOutside)
+  }
+
+  hide() {
+    this.el.classList.remove('active')
+    window.removeEventListener('click', this.clickedOutside)
+  }
+
+  clearInput() {
+    this.input.value = '';
   }
 
   destroy() {
-    this.button.removeEventListener('click' + this.namespace, this.showForm)
-    window.removeEventListener('click' + this.namespace, this.hideForm)
+    this.button.removeEventListener('click', this.clickedCTA)
+    window.removeEventListener('click', this.clickedOutside)
   }
 
-  _showForm(e) {
+  _clickedInside(e) {
     e.preventDefault()
     e.stopPropagation()
-    this.el.classList.add('active')
-    window.addEventListener('click' + namespace, this.hideForm)
   }
 
-  _hideForm(e) {
+  _clickedClear(e) {
+    if (this.input.value.length > 0) {
+      this.clearInput()
+    } else {
+      this.hide()
+    }
+  }
+
+  _clickedCTA(e) {
     e.preventDefault()
-    this.el.classList.remove('active')
-    window.removeEventListener('click' + namespace, this.hideForm)
+    e.stopPropagation()
+    this.show()
   }
 
+
+  _clickedOutside(e) {
+    e.preventDefault()
+    this.hide()
+  }
 }
